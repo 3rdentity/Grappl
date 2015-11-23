d6aec69959150dc08bf54eaa93f72e02fa704496
Matthew Hebert <soyrux@gmail.com>
2015-11-23 12:23:58 -0500

    Update HeadlessCompatClient

--- src/main/java/org/kneelawk/grappl/client/HeadlessCompatClient.java
+++ src/main/java/org/kneelawk/grappl/client/HeadlessCompatClient.java
@@ -3,7 +3,7 @@ package org.kneelawk.grappl.client;
 import io.grappl.client.api.Grappl;
 import io.grappl.client.impl.Application;
 import io.grappl.client.api.ApplicationMode;
-import io.grappl.client.impl.error.AuthenticationFailureException;
+import io.grappl.client.impl.error.AuthenticationException;
 import io.grappl.client.impl.stable.GrapplBuilder;
 import io.grappl.client.impl.error.RelayServerNotFoundException;
 
@@ -206,7 +206,7 @@ public class HeadlessCompatClient {
 				&& !"".equals(password)) {
             try {
                 builder.login(username, password.toCharArray(), null);
-            } catch (AuthenticationFailureException e) {
+            } catch (AuthenticationException e) {
                 e.printStackTrace();
             }
         }
